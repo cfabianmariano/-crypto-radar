18d71c7971768cbac1aa7cd9fca3d08080a17b89
nosoyunhacker <cfabianmariano@gmail.com>
2026-08-17 18:54:07 -0400

    Add supported crypto assets

---
+++ src/data/coins.js
@@ -0,0 +1,7 @@
+export const COINS = [
+  { id: 'bitcoin', symbol: 'BTC', name: 'Bitcoin' },
+  { id: 'ripple', symbol: 'XRP', name: 'XRP' },
+  { id: 'binancecoin', symbol: 'BNB', name: 'BNB' },
+  { id: 'ethereum', symbol: 'ETH', name: 'Ethereum' },
+  { id: 'theta-network', symbol: 'THETA', name: 'Theta Network' },
+]
