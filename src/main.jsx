1a5768f643d06063bca4e465b337d1892a1b1f81
nosoyunhacker <cfabianmariano@gmail.com>
2026-08-18 19:31:04 -0400

    Load heatmap dashboard styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './mobile-chart-compact.css'
+import './dashboard-heatmap.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
