e3b114976d76a44de0eddb08235a65dcda04f750
nosoyunhacker <cfabianmariano@gmail.com>
2026-08-19 11:48:22 -0400

    Activate tabbed iOS shell and PWA enhancer

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,13 @@ import App from './App'
 import './styles.css'
 import './mobile-chart-compact.css'
 import './dashboard-heatmap.css'
+import './app-shell.css'
+import { enhanceCryptoRadar } from './lib/appEnhancer'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+enhanceCryptoRadar()
